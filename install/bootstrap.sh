978f0c888c1e77c5d3db933a5cfbed90ecfac812
Denis Griffis <griffis.30@osu.edu>
2016-01-05 16:14:48 -0500

    Set up Splitta installation and patching

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -6,6 +6,7 @@ DATA=/vagrant/data
 
 ## Set up directories as necessary
 mkdir -p $INSTALL/ctakes
+mkdir -p $INSTALL/splitta/svm_light
 mkdir -p $DATA/i2b2/plaintext
 mkdir -p $DATA/i2b2/bounds
 mkdir -p $DATA/genia/plaintext
@@ -21,6 +22,7 @@ apt-get purge -y openjdk-6-jre-headless openjdk-6-jre-lib
 apt-get install -y unzip
 apt-get install -y openjdk-7-jdk
 apt-get install -y make
+apt-get install -y patch
 
 ## Install NLTK (for Switchboard parsing)
 wget https://bootstrap.pypa.io/ez_setup.py -O - | python
@@ -51,11 +53,37 @@ fi
 cp -R resources/* $CTAKESNAME/resources/
 echo "export CTAKES_HOME=`pwd`/$CTAKESNAME" >> $BASHRC
 
+## Install Splitta
+# (dependency: SVM_Light)
+SVMLIGHTPKG=svm_light_linux64.tar.gz
+SVMLIGHTURL="http://download.joachims.org/svm_light/current/$SVMLIGHTPKG"
+cd $INSTALL/splitta/svm_light
+if [ ! -e $SVMLIGHTPKG ]; then
+    wget $SVMLIGHTURL
+fi
+tar -xzf $SVMLIGHTPKG
+# (actual Splitta)
+SPLITTANAME=splitta.1.03
+SPLITTAPKG=splitta.1.03.tgz
+SPLITTAURL="https://splitta.googlecode.com/files/$SPLITTAPKG"
+cd $INSTALL/splitta
+if [ ! -e $SPLITTAPKG ]; then
+    wget $SPLITTAURL
+fi
+mkdir -p $SPLITTANAME
+cp $SPLITTAPKG $SPLITTANAME/
+cd $SPLITTANAME/
+tar -xzf $SPLITTAPKG
+rm $SPLITTAPKG
+cd ../
+# apply patch with code changes for SBD
+patch $SPLITTANAME/sbd.py < ${SPLITTANAME}.patch
+
 ## Download GENIA
 GENIANAME="GENIA_treebank_v1"
 GENIAURL="http://www.nactem.ac.uk/GENIA/current/GENIA-corpus/Treebank/$GENIANAME.tar.gz"
 
-if [ ! -e "$DATA/genia/$GENIANAME.tar.gz" ]; then
+if [ ! -e "$DATA/genia/${GENIANAME}.tar.gz" ]; then
     cd $DATA/genia
     wget $GENIAURL
 fi
